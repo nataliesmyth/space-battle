7faf187cc766df98e1accb61b28d531d3b130822
Nataliesmyth <nataliesmyth11@gmail.com>
2021-08-15 21:42:23 -0400

    added ranged properties to enemy ships

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Parent Ship Class
 
-class Ship {
-    constructor(hull, firepower, accuracy) {
-    //   this.id = id;
+class Player {
+    constructor(hull=20, firepower=5, accuracy=.7) {
+        //   this.id = id;
       this.hull = hull;
       this.firepower = firepower;
       this.accuracy = accuracy;
@@ -14,26 +14,29 @@ class Ship {
       }
 }
 
-class Player extends Ship {
-    constructor(hull=20, firepower=5, accuracy=.7) {
-        super(hull, firepower, accuracy)
-    }
-}
-
-const player = new Player();
-console.log(player)
 
 function getRandomNumber (min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 };
-console.log(getRandomNumber(2, 5))
 
-class Enemy extends Ship {
-    constructor(hull=10, firepower=10, accuracy=.6) {
-        super(hull, firepower, accuracy)
-        
+// we need a random decimal, not a number
+function getRandomDecimal (min, max) {
+    if ((min || max) >= 1 || (min || max) < 0) {
+        return 'Please use numbers that are between 0 and 1'
+    } else {
+        return Math.random() * (max - min) + min
     }
 }
+console.log(getRandomDecimal(.2, .5))
+
+class Enemy {
+    constructor(hull, firepower, accuracy) {
+        this.hull = getRandomNumber(3, 6);
+        this.firepower = getRandomNumber(2, 4);
+        this.accuracy = getRandomDecimal(.6, .8);
+    }
+}
+
 
 for (let i = 0; i < 6; i++) {
     const enemy = new Enemy();
